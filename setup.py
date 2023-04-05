1af4784b012255578f136213fd295722bb1e872f
Yuxiang Wu <yuxiang.cs@gmail.com>
2023-04-05 02:17:14 +0100

    v0.1.5

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ requirements = _deps
 
 setup(
     name="chatarena",
-    version="0.1.4",
+    version="0.1.5",
     author="Yuxiang Wu",
     author_email="yuxiang.cs@gmail.com",
     description="",
