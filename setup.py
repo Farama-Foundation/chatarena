080fea1dad6577cc46cfc658d14633b701df8def
Yuxiang Wu <yuxiang.cs@gmail.com>
2023-05-09 20:52:05 +0100

    Release v0.1.9: Improve OpenAI's agency (so it is less likely to confuse its role with others' now). Added new environment Pettingzoo Tic-tac-toe. Other bug fixes and improvements.

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ requirements = _deps
 
 setup(
     name="chatarena",
-    version="0.1.8.dev0",
+    version="0.1.9",
     author="Yuxiang Wu",
     author_email="yuxiang.cs@gmail.com",
     description="",
