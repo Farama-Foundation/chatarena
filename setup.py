d6a1f0e490d96bf54e814fb6cfff94fc8279b109
Yuxiang Wu <yuxiang.cs@gmail.com>
2023-05-26 20:18:58 +0100

    Releasing v0.1.10

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ requirements = _deps
 
 setup(
     name="chatarena",
-    version="0.1.9",
+    version="0.1.10",
     author="Yuxiang Wu",
     author_email="yuxiang.cs@gmail.com",
     description="",
