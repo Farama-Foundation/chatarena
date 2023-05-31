8a9c74bc9d5b369fa6709ce99f3ce7b676757b61
Yuxiang Wu <yuxiang.cs@gmail.com>
2023-06-01 00:32:21 +0100

    Make gradio, pettingzoo, and the backends as optional dependencies

--- setup.py
+++ setup.py
@@ -1,26 +1,27 @@
 from setuptools import setup, find_packages
 
-_deps = [
-    "openai==0.27.2",
-    "anthropic==0.2.8",
-    "cohere==4.3.1",
-    "transformers>=4.27.4",
-    "tenacity==8.2.2",
-    "gradio==3.20.0",
-    "rich==13.3.3",
-    "prompt_toolkit==3.0.38",
-    "pettingzoo==1.23.0",
-    "chess==1.9.4",
-]
-
 with open("README.md", "r") as f:
     long_description = f.read()
 
-requirements = _deps
+base_requirements = [
+    "openai>=0.27.2",
+    "tenacity==8.2.2",
+    "rich==13.3.3",
+    "prompt_toolkit==3.0.38",
+]
+anthropic_requirements = ["anthropic>=0.2.8"]
+cohere_requirements = ["cohere>=4.3.1"]
+hf_requirements = ["transformers>=4.27.4"]
+gradio_requirements = ["gradio==3.20.0"]
+pettingzoo_requirements = ["pettingzoo==1.23.0", "chess==1.9.4"]
+
+all_backends = anthropic_requirements + cohere_requirements + hf_requirements
+all_requirements = anthropic_requirements + cohere_requirements + hf_requirements + \
+                   gradio_requirements + pettingzoo_requirements
 
 setup(
     name="chatarena",
-    version="0.1.10",
+    version="0.1.10.1",
     author="Yuxiang Wu",
     author_email="yuxiang.cs@gmail.com",
     description="",
@@ -34,5 +35,14 @@ setup(
         "Topic :: Scientific/Engineering :: Artificial Intelligence",
     ],
     python_requires=">=3.7",
-    install_requires=requirements,
+    install_requires=base_requirements,
+    extras_require={
+        "anthropic": anthropic_requirements,
+        "cohere": cohere_requirements,
+        "huggingface": hf_requirements,
+        "pettingzoo": pettingzoo_requirements,
+        "gradio": gradio_requirements,
+        "all_backends": all_backends,
+        "all": all_requirements,
+    },
 )
