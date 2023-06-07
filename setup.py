9992a8b21de3abe1bd8c96c9045aae23e575a15f
Yuxiang Wu <yuxiang.cs@gmail.com>
2023-06-07 15:18:36 +0100

    Release v0.1.12: added tutorials for umshini and langchain agents

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ anthropic_requirements = ["anthropic>=0.2.8"]
 cohere_requirements = ["cohere>=4.3.1"]
 hf_requirements = ["transformers>=4.27.4"]
 bard_requirements = ["bardapi==0.1.11"]
-langchain_requirements = ["langchain>=0.0.170"]
+langchain_requirements = ["langchain>=0.0.135"]
 gradio_requirements = ["gradio==3.20.0"]
 pettingzoo_requirements = ["pettingzoo==1.23.0", "chess==1.9.4"]
 umshini_requirements = ["pygame==2.4.0"] + pettingzoo_requirements + langchain_requirements
@@ -32,7 +32,7 @@ all_requirements = all_backends + all_envs + gradio_requirements
 
 setup(
     name="chatarena",
-    version="0.1.11",
+    version="0.1.12",
     author="Yuxiang Wu",
     author_email="yuxiang.cs@gmail.com",
     description="",
