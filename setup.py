8e43e1a200da18b0d5d76ede83d0f4bd70729f2f
Yuxiang Wu <yuxiang.cs@gmail.com>
2023-06-21 22:07:06 +0100

    Release v0.1.12.6 to revert the revert of #47.

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ all_requirements = all_backends + all_envs + gradio_requirements
 
 setup(
     name="chatarena",
-    version="0.1.12.5",
+    version="0.1.12.6",
     author="Yuxiang Wu",
     author_email="yuxiang.cs@gmail.com",
     description="",
