7cf3167562e7b22ca10e12251e8677e31e10dc04
Elliot Tower <elliot@elliottower.com>
2023-11-14 19:30:13 -0500

    Make version done dynamically in init, access for windows to install with sdist test

--- chatarena/__init__.py
+++ chatarena/__init__.py
@@ -4,3 +4,5 @@ ROOT_DIR = (
     os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir)) + os.path.sep
 )
 EXAMPLES_DIR = os.path.join(ROOT_DIR, "examples")
+
+__version__ = "0.1.12.12"
--- setup.py
+++ setup.py
@@ -7,4 +7,15 @@ from setuptools import setup
 CWD = pathlib.Path(__file__).absolute().parent
 
 
-setup(name="chatarena")
+def get_version():
+    """Gets the chatarena version."""
+    path = CWD / "chatarena" / "__init__.py"
+    content = path.read_text()
+
+    for line in content.splitlines():
+        if line.startswith("__version__"):
+            return line.strip().split()[-1].strip().strip('"')
+    raise RuntimeError("bad version data in __init__.py")
+
+
+setup(name="chatarena", version=get_version())
